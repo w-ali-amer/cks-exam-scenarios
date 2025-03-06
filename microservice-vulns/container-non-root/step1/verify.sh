3311729936fa081a1536477f68a4ac940b7aaf53
w-ali-amer <48703452+w-ali-amer@users.noreply.github.com>
2025-03-06 06:32:57 +0300

    Update verify.sh
    the jsonpath for fetching the security context was wrong

--- microservice-vulns/container-non-root/step1/verify.sh
+++ microservice-vulns/container-non-root/step1/verify.sh
@@ -2,7 +2,7 @@
 
 # Verify that the Pod is running as a non-root user
 pod_status=$(kubectl get pod dark-illusion -n limited -o jsonpath='{.status.phase}')
-run_as=$(kubectl get pod dark-illusion -n limited -o jsonpath='{.spec.containers[0].securityContext.runAsNonRoot}')
+run_as=$(kubectl get pod dark-illusion -n limited -o jsonpath='{.spec.securityContext.runAsNonRoot}')
 res=$(cat /opt/non-root/answer)
 
 if [[ "$pod_status" == "Running" && "$run_as" == "true" ]]; then
